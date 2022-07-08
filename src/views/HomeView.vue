bff9844a799bbd67d0311ea5a6fce6277a88076c
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-07-08 23:53:41 +0800

    feat: added dashboard

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,36 +1,10 @@
 <template>
 	<div class="home">
-		<div class="card p-4">
-			<h3>Item List</h3>
-			<table class="table table-bordered">
-				<thead>
-					<tr>
-						<th scope="col">#</th>
-						<th scope="col">First</th>
-						<th scope="col">Last</th>
-						<th scope="col">Handle</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th scope="row">1</th>
-						<td>Mark</td>
-						<td>Otto</td>
-						<td>@mdo</td>
-					</tr>
-					<tr>
-						<th scope="row">2</th>
-						<td>Jacob</td>
-						<td>Thornton</td>
-						<td>@fat</td>
-					</tr>
-					<tr>
-						<th scope="row">3</th>
-						<td colspan="2">Larry the Bird</td>
-						<td>@twitter</td>
-					</tr>
-				</tbody>
-			</table>
+		<div class="card p-4 border-0" style="margin-top: 2rem">
+			<div class="d-flex justify-content-between align-items-center">
+				<h5 class="card-title mb-4">Dashboard</h5>
+			</div>
+			Welcome!
 		</div>
 	</div>
 </template>
