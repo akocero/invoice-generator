7ba4ed6af7180a68cfd3213dc07875f0930ccc55
Eugene Paul Badato <badatoeugenepaulm@gmail.com>
2022-07-09 01:29:12 +0800

    feat: added generate invoice logic

--- src/views/CreateInvoice.vue
+++ src/views/CreateInvoice.vue
@@ -12,109 +12,138 @@
 								Print Invoice
 							</button>
 						</div>
-						<div class="row">
-							<div class="mb-3 col-6">
-								<label for="" class="form-label"
-									>Customers</label
-								>
-								<select
-									class="form-select"
-									aria-label="Default select example"
-								>
-									<option selected>
-										Open this select menu
-									</option>
-									<option value="1">One</option>
-									<option value="2">Two</option>
-									<option value="3">Three</option>
-								</select>
+						<form @submit.prevent="addItem">
+							<div class="row">
+								<div class="mb-3 col-6">
+									<label for="" class="form-label"
+										>Customers</label
+									>
+									<select
+										class="form-select"
+										aria-label="Default select example"
+										required
+										v-model="customer"
+									>
+										<option selected value="">
+											Select...
+										</option>
+
+										<option
+											:value="itemCustomer.id"
+											v-for="itemCustomer in customers"
+											:key="itemCustomer.id"
+										>
+											{{ itemCustomer.name }}
+										</option>
+									</select>
+								</div>
+								<div class="mb-3 col-6">
+									<label for="" class="form-label"
+										>Payable To</label
+									>
+									<select
+										class="form-select"
+										aria-label="Default select example"
+										required
+										v-model="payableTo"
+									>
+										<option selected value="">
+											Select...
+										</option>
+
+										<option
+											:value="itemPayable.id"
+											v-for="itemPayable in payables"
+											:key="itemPayable.id"
+										>
+											{{ itemPayable.name }}
+										</option>
+									</select>
+								</div>
+								<div class="mb-3 col-6">
+									<label
+										for="exampleFormControlInput1"
+										class="form-label"
+										>Due Date</label
+									>
+									<input
+										type="date"
+										class="form-control"
+										id="exampleFormControlInput1"
+									/>
+								</div>
+								<div class="mb-3 col-6">
+									<label
+										for="exampleFormControlInput1"
+										class="form-label"
+										>Discount Percent</label
+									>
+									<input
+										type="number"
+										class="form-control"
+										id="exampleFormControlInput1"
+									/>
+								</div>
+								<div class="mb-3">
+									<label
+										for="exampleFormControlTextarea1"
+										class="form-label"
+										>Notes</label
+									>
+									<textarea
+										class="form-control"
+										id="exampleFormControlTextarea1"
+										rows="3"
+									></textarea>
+								</div>
+								<div class="mb-3 col-6">
+									<label for="" class="form-label"
+										>Items</label
+									>
+									<select
+										class="form-select"
+										aria-label="Default select example"
+										required
+										v-model="selectedItem"
+									>
+										<option selected value="">
+											Select...
+										</option>
+
+										<option
+											:value="itemDetail.id"
+											v-for="itemDetail in items"
+											:key="itemDetail.id"
+										>
+											{{ itemDetail.name }}
+										</option>
+									</select>
+								</div>
+								<div class="mb-3 col-6">
+									<label
+										for="exampleFormControlInput1"
+										class="form-label"
+										>Quantity</label
+									>
+									<input
+										required
+										type="number"
+										class="form-control"
+										id="exampleFormControlInput1"
+										v-model="quantity"
+									/>
+								</div>
 							</div>
-							<div class="mb-3 col-6">
-								<label for="" class="form-label"
-									>Payable To</label
-								>
-								<select
-									class="form-select"
-									aria-label="Default select example"
-								>
-									<option selected>
-										Open this select menu
-									</option>
-									<option value="1">One</option>
-									<option value="2">Two</option>
-									<option value="3">Three</option>
-								</select>
+							<div class="row">
+								<div class="col-12">
+									<input
+										class="btn btn-success"
+										value="Add Item"
+										type="submit"
+									/>
+								</div>
 							</div>
-							<div class="mb-3 col-6">
-								<label
-									for="exampleFormControlInput1"
-									class="form-label"
-									>Due Date</label
-								>
-								<input
-									type="date"
-									class="form-control"
-									id="exampleFormControlInput1"
-								/>
-							</div>
-							<div class="mb-3 col-6">
-								<label
-									for="exampleFormControlInput1"
-									class="form-label"
-									>Discount Percent</label
-								>
-								<input
-									type="number"
-									class="form-control"
-									id="exampleFormControlInput1"
-								/>
-							</div>
-							<div class="mb-3">
-								<label
-									for="exampleFormControlTextarea1"
-									class="form-label"
-									>Notes</label
-								>
-								<textarea
-									class="form-control"
-									id="exampleFormControlTextarea1"
-									rows="3"
-								></textarea>
-							</div>
-							<div class="mb-3 col-6">
-								<label for="" class="form-label">Items</label>
-								<select
-									class="form-select"
-									aria-label="Default select example"
-								>
-									<option selected>
-										Open this select menu
-									</option>
-									<option value="1">One</option>
-									<option value="2">Two</option>
-									<option value="3">Three</option>
-								</select>
-							</div>
-							<div class="mb-3 col-6">
-								<label
-									for="exampleFormControlInput1"
-									class="form-label"
-									>Quantity</label
-								>
-								<input
-									type="number"
-									class="form-control"
-									id="exampleFormControlInput1"
-								/>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-12">
-								<button class="btn btn-success">
-									Add Item
-								</button>
-							</div>
-						</div>
+						</form>
 					</div>
 				</div>
 			</div>
@@ -132,33 +161,33 @@
 								</tr>
 							</thead>
 							<tbody>
-								<tr>
-									<th scope="row">Item 1</th>
-									<td>1</td>
-									<td>50</td>
+								<tr v-for="i in addedItems" :key="i.id">
+									<th scope="row">{{ i.name }}</th>
+									<td>{{ i.qty }}</td>
+									<td>
+										₱{{ numberWithCommas(i.unitPrice) }}
+									</td>
 									<td>
 										<button
 											class="btn btn-sm btn-outline-secondary"
+											@click="deleteAddedItem(i.id)"
 										>
 											Delete
 										</button>
 									</td>
 								</tr>
-								<tr>
-									<th scope="row">Item 2</th>
-									<td>1</td>
-									<td>50</td>
-									<td>
-										<button
-											class="btn btn-sm btn-outline-secondary"
-										>
-											Delete
-										</button>
-									</td>
-								</tr>
-								<tr>
+
+								<tr v-if="addedItems.length">
 									<td><strong>Total</strong></td>
-									<td colspan="3">100</td>
+									<td colspan="3">
+										<strong
+											>₱{{
+												numberWithCommas(
+													addedItemsTotal
+												)
+											}}</strong
+										>
+									</td>
 								</tr>
 							</tbody>
 						</table>
@@ -170,7 +199,115 @@
 </template>
 
 <script>
-export default {};
+import { ref } from 'vue';
+export default {
+	setup() {
+		const items = ref([
+			{
+				id: 1,
+				name: 'item 1',
+				unitPrice: 10
+			},
+			{
+				id: 2,
+				name: 'item 2',
+				unitPrice: 15
+			}
+		]);
+		const addedItems = ref([]);
+		const addedItemsTotal = ref(0);
+		const customers = ref([
+			{
+				id: 1,
+				name: 'customer 1',
+				details: 'customer 1 details'
+			},
+			{
+				id: 2,
+				name: 'customer 2',
+				details: 'customer 2 details'
+			}
+		]);
+		const payables = ref([
+			{
+				id: 1,
+				name: 'payable 1',
+				details: 'payable 1 details'
+			},
+			{
+				id: 2,
+				name: 'payable 2',
+				details: 'payable 2 details'
+			}
+		]);
+		const customer = ref(1);
+		const payableTo = ref(1);
+		const dueDate = ref('');
+		const discount = ref('');
+		const notes = ref('');
+		const selectedItem = ref('');
+		const quantity = ref('');
+
+		const addItem = () => {
+			const _item = items.value.find(
+				(item) => item.id === selectedItem.value
+			);
+			const isItemExist = addedItems.value.find(
+				(el) => _item.id === el.id
+			);
+			if (!isItemExist) {
+				addedItems.value.push({
+					id: _item.id,
+					name: _item.name,
+					unitPrice: _item.unitPrice,
+					qty: quantity.value
+				});
+			} else {
+				alert('already added');
+			}
+
+			selectedItem.value = '';
+			quantity.value = '';
+			getTotal();
+		};
+
+		const getTotal = () => {
+			let _total = 0;
+			addedItems.value.forEach((el) => {
+				_total += el.unitPrice * el.qty;
+			});
+
+			addedItemsTotal.value = _total;
+		};
+
+		const deleteAddedItem = (id) => {
+			addedItems.value = addedItems.value.filter((el) => el.id !== id);
+			getTotal();
+		};
+
+		const numberWithCommas = (x) => {
+			return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+		};
+
+		return {
+			addItem,
+			customer,
+			payableTo,
+			dueDate,
+			discount,
+			notes,
+			selectedItem,
+			quantity,
+			items,
+			customers,
+			payables,
+			addedItems,
+			addedItemsTotal,
+			deleteAddedItem,
+			numberWithCommas
+		};
+	}
+};
 </script>
 
 <style></style>
